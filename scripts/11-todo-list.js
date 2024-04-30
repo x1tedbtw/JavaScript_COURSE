384313229860d986f7b4332ed9543afd95e7a7a7
x1tedbtw <nefedov.vitali@gmail.com>
2024-04-30 12:10:01 +0200

    finished Todo list project
    learned about:
    arrays, loops, accumulator pattern, references, destructuring, break, continue, loop inside function

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,15 +1,24 @@
-const todoList = ["amke", "wash dishes"];
+const todoList = [];
 
 renderTodoList();
 function renderTodoList() {
     let todoListHTML = '';
 
     for (let i= 0; i < todoList.length;i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`; // Generating HTML
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        // const dueDate = todoObject.dueDate;
+        const {name, dueDate} = todoObject; // Destructuring
+        const html = `
+            <div>${name}</div>    
+            <div>${dueDate}</div> 
+            <button onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>
+        `; // Generating HTML
         todoListHTML += html
     }
-    console.log(todoListHTML)
 
     document.querySelector('.js-todo-list')
         .innerHTML = todoListHTML;
@@ -21,8 +30,18 @@ function addTodo() {
     const inputElement = document.querySelector('.js-name-input')
     const name = inputElement.value;
 
-    todoList.push(name);
-    console.log(todoList);
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+
+
+    todoList.push({
+        // name: name,
+        // dueDate:dueDate,
+        name,
+        dueDate
+    });
+
 
     inputElement.value = '';
 

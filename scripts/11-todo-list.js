1b548fba35c9da97ad78db2c6fe37c6f4a3c298a
x1tedbtw <nefedov.vitali@gmail.com>
2024-04-30 10:59:04 +0200

    Arrays and loops practice

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,21 @@
-const todoList = [];
+const todoList = ["amke", "wash dishes"];
+
+renderTodoList();
+function renderTodoList() {
+    let todoListHTML = '';
+
+    for (let i= 0; i < todoList.length;i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`; // Generating HTML
+        todoListHTML += html
+    }
+    console.log(todoListHTML)
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
+
+
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input')
@@ -8,5 +25,7 @@ function addTodo() {
     console.log(todoList);
 
     inputElement.value = '';
+
+    renderTodoList();
 
 }

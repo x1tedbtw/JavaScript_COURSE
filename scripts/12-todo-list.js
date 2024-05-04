64ba7f74fb930aa14b97d1fcb0e4ce02b1b95238
x1tedbtw <nefedov.vitali@gmail.com>
2024-05-04 17:02:54 +0200

    MAKING PROGRESS: - Added forEach function instead of for loop for to do list project - Learned how to 'continue' using 'return'

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,48 @@
+const todoList = [];
+
+renderTodoList();
+function renderTodoList() {
+    let todoListHTML = '';
+
+
+    todoList.forEach(function(todoObject, index) {
+        const {name, dueDate} = todoObject; // Destructuring
+        const html = `
+            <div>${name}</div>    
+            <div>${dueDate}</div> 
+            <button onclick="
+                todoList.splice(${index}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>
+        `; // Generating HTML
+        todoListHTML += html
+    });
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
+
+
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input')
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+
+
+    todoList.push({
+        // name: name,
+        // dueDate:dueDate,
+        name,
+        dueDate
+    });
+
+
+    inputElement.value = '';
+
+    renderTodoList();
+
+}
